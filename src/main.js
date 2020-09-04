84682cafc3fc72c070a72b620dac6e5451008eaa
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-04 16:12:58 -0700

    update styling to center text, add info text, and refer to planet's years as their respective names

--- src/main.js
+++ src/main.js
@@ -12,28 +12,28 @@ $(document).ready(function(){
     let user = new User(inputtedAge, inputtedExpectancy);
     $(".info-screen").show();
 
-    $("#age-on-mercury").text(`Age on Mercury: ${user.mercuryAge()} years.`);
+    $("#age-on-mercury").text(`Age on Mercury: ${user.mercuryAge()} Mercurian years.`);
     $("#years-left-mercury").text(user.mercuryYearsLeft());
 
-    $("#age-on-venus").text(`Age on Venus: ${user.venusAge()} years.`);
+    $("#age-on-venus").text(`Age on Venus: ${user.venusAge()} Venusian years.`);
     $("#years-left-venus").text(user.venusYearsLeft());
 
-    $("#age-on-mars").text(`Age on Mars: ${user.marsAge()} years.`);
+    $("#age-on-mars").text(`Age on Mars: ${user.marsAge()} Martian years.`);
     $("#years-left-mars").text(user.marsYearsLeft());
 
-    $("#age-on-jupiter").text(`Age on Jupiter: ${user.jupiterAge()} years.`);
+    $("#age-on-jupiter").text(`Age on Jupiter: ${user.jupiterAge()} Jovian years.`);
     $("#years-left-jupiter").text(user.jupiterYearsLeft());
 
-    $("#age-on-saturn").text(`Age on Saturn: ${user.saturnAge()} years.`);
+    $("#age-on-saturn").text(`Age on Saturn: ${user.saturnAge()} Saturnian years.`);
     $("#years-left-saturn").text(user.saturnYearsLeft());
     
-    $("#age-on-uranus").text(`Age on Uranus: ${user.uranusAge()} years.`);
+    $("#age-on-uranus").text(`Age on Uranus: ${user.uranusAge()} Uranian years.`);
     $("#years-left-uranus").text(user.uranusYearsLeft());
 
-    $("#age-on-neptune").text(`Age on Neptune: ${user.neptuneAge()} years.`);
+    $("#age-on-neptune").text(`Age on Neptune: ${user.neptuneAge()} Neptunian years.`);
     $("#years-left-neptune").text(user.neptuneYearsLeft());
 
-    $("#age-on-pluto").text(`Age on Pluto: ${user.plutoAge()} years.`);
+    $("#age-on-pluto").text(`Age on Pluto: ${user.plutoAge()} Plutonian years.`);
     $("#years-left-pluto").text(user.plutoYearsLeft());
   });
 });
