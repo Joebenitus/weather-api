be917e55cf82b18ac83d0921bde9f2c9aee1a253
Joseph Karnafel <josephkarnafel@gmail.com>
2020-09-04 10:53:43 -0700

    add good fail test for Venus life expectancy prototype method

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -8,22 +8,22 @@ describe('User', () => {
 
   test('should output the user\'s age on Mercury', () => {
     let user = new User(40);
-    expect(user.mercuryAge(user.age)).toEqual("166.67");
+    expect(user.mercuryAge()).toEqual("166.67");
   });
 
   test('should output the user\'s age on Venus', () => {
     let user = new User(40);
-    expect(user.venusAge(user.age)).toEqual("64.52");
+    expect(user.venusAge()).toEqual("64.52");
   });
 
   test('should output the user\'s age on Mars', () => {
     let user = new User(40);
-    expect(user.marsAge(user.age)).toEqual("21.28");
+    expect(user.marsAge()).toEqual("21.28");
   });
 
   test('should output the user\'s age on Jupiter', () => {
     let user = new User(40);
-    expect(user.jupiterAge(user.age)).toEqual("3.37");
+    expect(user.jupiterAge()).toEqual("3.37");
   });
 
   test('should take the user\'s life expectancy as parameter of user object', () => {
@@ -33,6 +33,11 @@ describe('User', () => {
 
   test('should calculate the years the user has left to live on Mercury', () => {
     let user = new User(40, 50);
-    expect(user.mercuryYearsLeft(user.age, user.lifeExpect)).toEqual("41.67");
+    expect(user.mercuryYearsLeft()).toEqual("41.67");
+  });
+
+  test('should calculate the years the user has left to live on Venus', () => {
+    let user = new User(40, 50);
+    expect(user.venusYearsLeft()).toEqual("41.67");
   });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,23 +4,27 @@ export default class User{
     this.lifeExpect = lifeExpect;
   }
 
-  mercuryAge(age){
-    return (age / 0.24).toFixed(2);
+  mercuryAge(){
+    return (this.age / 0.24).toFixed(2);
   }
 
-  venusAge(age){
-    return (age / 0.62).toFixed(2);
+  venusAge(){
+    return (this.age / 0.62).toFixed(2);
   }
 
-  marsAge(age){
-    return (age / 1.88).toFixed(2);
+  marsAge(){
+    return (this.age / 1.88).toFixed(2);
   }
 
-  jupiterAge(age){
-    return (age / 11.86).toFixed(2);
+  jupiterAge(){
+    return (this.age / 11.86).toFixed(2);
   }
 
   mercuryYearsLeft(){
     return ((this.lifeExpect - this.age) / 0.24).toFixed(2);
+  }
+
+  venusYearsLeft(){
+
   }
 }
